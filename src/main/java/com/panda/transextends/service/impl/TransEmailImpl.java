6bec3d9e6e16552afe9e315e7aed9741ecf29000
coolwhite520 <81929154@qq.com>
2022-03-03 15:39:35 +0800

    邮件的翻译也通过java进行解析

--- src/main/java/com/panda/transextends/service/impl/TransEmailImpl.java
+++ src/main/java/com/panda/transextends/service/impl/TransEmailImpl.java
@@ -1,19 +1,174 @@
 package com.panda.transextends.service.impl;
 
 import com.panda.transextends.service.TransFile;
+import com.panda.transextends.utils.CoreApi;
 import com.panda.transextends.utils.PluginsApi;
+import org.apache.commons.mail.util.MimeMessageParser;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.mail.Address;
+import javax.mail.BodyPart;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.*;
 
 @Service
 public class TransEmailImpl implements TransFile {
 
     @Autowired
-    PluginsApi pluginsApi;
+    CoreApi coreApi;
+
+    //            //邮件唯一id
+//            String messageId = parser.getMimeMessage().getMessageID();
+//            System.out.println(messageId);
+//            //发件人
+//            String from = parser.getFrom();
+//            System.out.println(from);
+//            //收件人列表
+//            List<Address> toArray = parser.getTo();
+//            System.out.println(toArray);
+//            //抄送人列表
+//            List<Address> ccArray = parser.getCc();
+//            System.out.println(ccArray);
+//            //密送人列表
+//            List<Address> bccArray = parser.getBcc();
+//            System.out.println(bccArray);
+//            //邮件发送时间
+//            Date sendDate = parser.getMimeMessage().getSentDate();
+//            System.out.println(sendDate);
+//            //邮件主题
+//            String subject = parser.getSubject();
+//            System.out.println(subject);
+
+    public long calculateTotalProgress(String srcLang, String srcFile) throws Exception {
+        long total = 0;
+        InputStream inputStream = new FileInputStream(srcFile);
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+        MimeMessage msg = new MimeMessage(session, inputStream);
+        MimeMessageParser parser = new MimeMessageParser(msg).parse();
+        String html = parser.getHtmlContent();
+        List<DataSource> dataSources = parser.getAttachmentList();
+        //获取不到html内容时，则获取非html文本内容
+        if (html == null || html.length() == 0) {
+            String plain = parser.getPlainContent();
+            ArrayList<List<String>> list = coreApi.tokenize(srcLang, plain);
+            for (List<String> strings : list) {
+                for (String string : strings) {
+                    total++;
+                }
+            }
+        } else {
+            //解析html 并翻译
+            Document document = Jsoup.parse(html);
+            Element entry = document.select("body").first();
+            Elements tags = entry.getAllElements();
+            for (Element tag : tags) {
+                for (Node child : tag.childNodes()) {
+                    if (child instanceof TextNode && !((TextNode) child).isBlank()) {
+                        total++;
+                    }
+                }
+            }
+        }
+        inputStream.close();
+        return total;
+    }
 
     @Override
     public boolean translate(int rowId, String srcLang, String desLang, String srcFile, String desFile) throws Exception {
-//        托管给plugins容器用python模块进行处理,plugins模块采用python编写，能用java处理的就在当前模块处理，处理不了的就仍出去。
-        return pluginsApi.translate_email(rowId, srcLang, desLang, srcFile, desFile);
+        InputStream inputStream = new FileInputStream(srcFile);
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+        MimeMessage msg = new MimeMessage(session, inputStream);
+        MimeMessageParser parser = new MimeMessageParser(msg).parse();
+        //获取正文
+        String html = parser.getHtmlContent();
+        List<DataSource> dataSources = parser.getAttachmentList();
+        //获取不到html内容时，则获取非html文本内容
+        if (html == null || html.length() == 0) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String plain = parser.getPlainContent();
+            ArrayList<List<String>> list = coreApi.tokenize(srcLang, plain);
+            for (List<String> strings : list) {
+                for (String s : strings) {
+                    String transContent = coreApi.translate(srcLang, desLang, s);
+                    stringBuilder.append(transContent);
+                }
+            }
+            msg.setContent(stringBuilder.toString(), "text/plain");
+        } else {
+            //解析html 并翻译
+            Document document = Jsoup.parse(html);
+            Element entry = document.select("body").first();
+            Elements tags = entry.getAllElements();
+            for (Element tag : tags) {
+                for (Node child : tag.childNodes()) {
+                    if (child instanceof TextNode && !((TextNode) child).isBlank()) {
+                        String text = ((TextNode) child).text();
+                        text = coreApi.translate(srcLang, desLang, text);
+                        ((TextNode) child).text(text); //replace to word
+                    }
+                }
+            }
+            Multipart multipart = new MimeMultipart("mixed");// mixed表示混合性，这里因为有文本，附件，所以是混合的。
+            msg.setContent(multipart);
+
+            BodyPart htmlPart = new MimeBodyPart();
+            multipart.addBodyPart(htmlPart);
+
+            Collection<String> contentIds = parser.getContentIds(); // 获取内嵌html的图片
+            Multipart contentMulti;
+            if (contentIds.size() > 0) {
+                contentMulti = new MimeMultipart("related");//这里的图片和文本是在一起显示的所以他们是关系型的。
+            } else {
+                contentMulti = new MimeMultipart();
+            }
+            htmlPart.setContent(contentMulti);
+
+            // 添加正文
+            BodyPart textBody = new MimeBodyPart();
+            contentMulti.addBodyPart(textBody);
+            textBody.setContent(document.html(), "text/html");
+
+            //添加html图片
+            for (String contentId : contentIds) {
+                BodyPart jpgBody = new MimeBodyPart();
+                DataSource pic = parser.findAttachmentByCid(contentId);
+                jpgBody.setDataHandler(new DataHandler(pic));
+                jpgBody.setHeader("Content-ID", contentId);
+                contentMulti.addBodyPart(jpgBody);
+                dataSources.remove(pic);
+            }
+
+            // 添加附件
+            for (DataSource source : dataSources) {
+                MimeBodyPart attachmentPart = new MimeBodyPart();
+                attachmentPart.setDataHandler(new DataHandler(source));
+                attachmentPart.setFileName(source.getName());
+                multipart.addBodyPart(attachmentPart);
+            }
+        }
+        msg.saveChanges();
+        FileOutputStream fileOutputStream = new FileOutputStream(desFile);
+        msg.writeTo(fileOutputStream);
+        inputStream.close();
+        fileOutputStream.close();
+        return true;
     }
 }
--- src/test/java/com/panda/transextends/TransExtendsApplicationTests.java
+++ src/test/java/com/panda/transextends/TransExtendsApplicationTests.java
@@ -1,5 +1,6 @@
 package com.panda.transextends;
 
+
 import com.panda.transextends.pojo.OcrEntity;
 import com.panda.transextends.service.impl.TransDocxImpl;
 import com.panda.transextends.service.impl.TransImagesImpl;
@@ -7,19 +8,40 @@ import com.panda.transextends.service.impl.TransTikaImpl;
 import com.panda.transextends.utils.CoreApi;
 import com.panda.transextends.utils.OcrApi;
 import com.panda.transextends.utils.RedisUtil;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.mail.util.MimeMessageParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.File;
-import java.io.IOException;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Address;
+import javax.mail.BodyPart;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.Collection;
+import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 
 @SpringBootTest
 class TransExtendsApplicationTests {
@@ -66,11 +88,11 @@ class TransExtendsApplicationTests {
     @Test
     void testNewPage() throws IOException {
         PDDocument document = new PDDocument();
-        for (int i=0; i<10; i++) {
+        for (int i = 0; i < 10; i++) {
             //Creating a blank page
             PDPage blankPage = new PDPage();
             //Adding the blank page to the document
-            document.addPage( blankPage );
+            document.addPage(blankPage);
         }
         //Saving the document
         document.save("/Users/baiyang/Desktop/1.pdf");
@@ -90,7 +112,7 @@ class TransExtendsApplicationTests {
         //Begin the Content stream
         contentStream.beginText();
         //Setting the font to the Content stream
-        contentStream.setFont( PDType1Font.TIMES_ROMAN, 16);
+        contentStream.setFont(PDType1Font.TIMES_ROMAN, 16);
         //Setting the leading
         contentStream.setLeading(14.5f);
         //Setting the position for the line
@@ -104,5 +126,172 @@ class TransExtendsApplicationTests {
         doc.save(new File("/Users/baiyang/Desktop/text2.pdf"));
         //Closing the document
         doc.close();
+    }
+
+    @Test
+    void testEml() {
+        String strFile = "/Users/baiyang/Desktop/翻译资源/eml/今天.eml";
+        String desFile = "/Users/baiyang/Desktop/翻译资源/eml/今天_out.eml";
+        String dir = "/Users/baiyang/Desktop/翻译资源/eml/";
+        try (InputStream inputStream = new FileInputStream(strFile)) {
+            Properties props = new Properties();
+            Session session = Session.getDefaultInstance(props, null);
+            MimeMessage msg = new MimeMessage(session, inputStream);
+            MimeMessageParser parser = new MimeMessageParser(msg).parse();
+            //邮件唯一id
+            String messageId = parser.getMimeMessage().getMessageID();
+            System.out.println(messageId);
+            //发件人
+            String from = parser.getFrom();
+            System.out.println(from);
+            //收件人列表
+            List<Address> toArray = parser.getTo();
+            System.out.println(toArray);
+            //抄送人列表
+            List<Address> ccArray = parser.getCc();
+            System.out.println(ccArray);
+            //密送人列表
+            List<Address> bccArray = parser.getBcc();
+            System.out.println(bccArray);
+            //邮件发送时间
+            Date sendDate = parser.getMimeMessage().getSentDate();
+            System.out.println(sendDate);
+            //邮件主题
+            String subject = parser.getSubject();
+            System.out.println(subject);
+            //获取正文
+            String html = parser.getHtmlContent();
+            List<DataSource> dataSources = parser.getAttachmentList();
+            //获取不到html内容时，则获取非html文本内容
+            if (html == null || html.length() == 0) {
+                String plain = parser.getPlainContent();
+                plain = "word";
+                msg.setContent(plain, "text/plain");
+            } else {
+                //解析html 并翻译
+                Document document = Jsoup.parse(html);
+                Element entry = document.select("body").first();
+                Elements tags = entry.getAllElements();
+                for (Element tag : tags) {
+                    for (Node child : tag.childNodes()) {
+                        if (child instanceof TextNode && !((TextNode) child).isBlank()) {
+                            ((TextNode) child).text("word"); //replace to word
+                        }
+                    }
+                }
+                Multipart multipart = new MimeMultipart("mixed");// mixed表示混合性，这里因为有文本，附件，所以是混合的。
+                msg.setContent(multipart);
+
+                BodyPart htmlPart = new MimeBodyPart();
+                multipart.addBodyPart(htmlPart);
+
+                Collection<String> contentIds = parser.getContentIds(); // 获取内嵌html的图片
+                Multipart contentMulti;
+                if (contentIds.size() > 0) {
+                    contentMulti = new MimeMultipart("related");//这里的图片和文本是在一起显示的所以他们是关系型的。
+                } else {
+                    contentMulti = new MimeMultipart();
+                }
+                htmlPart.setContent(contentMulti);
+
+                // 添加正文
+                BodyPart textBody = new MimeBodyPart();
+                contentMulti.addBodyPart(textBody);
+                textBody.setContent(document.html(), "text/html");
+
+                //添加html图片
+                for (String contentId : contentIds) {
+                    BodyPart jpgBody = new MimeBodyPart();
+                    DataSource pic = parser.findAttachmentByCid(contentId);
+                    jpgBody.setDataHandler(new DataHandler(pic));
+                    jpgBody.setHeader("Content-ID", contentId);
+                    contentMulti.addBodyPart(jpgBody);
+                    dataSources.remove(pic);
+                }
+
+                // 添加附件
+                for (DataSource source : dataSources) {
+                    MimeBodyPart attachmentPart = new MimeBodyPart();
+                    attachmentPart.setDataHandler(new DataHandler(source));
+                    attachmentPart.setFileName(source.getName());
+                    multipart.addBodyPart(attachmentPart);
+                }
+            }
+            msg.saveChanges();
+            FileOutputStream fileOutputStream = new FileOutputStream(desFile);
+            msg.writeTo(fileOutputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    void testEmlOld() {
+        String strFile = "/Users/baiyang/Desktop/翻译资源/eml/aa.eml";
+        String desFile = "/Users/baiyang/Desktop/翻译资源/eml/aa_out.eml";
+        String dir = "/Users/baiyang/Desktop/翻译资源/eml/";
+        try (InputStream inputStream = new FileInputStream(strFile)) {
+            Properties props = new Properties();
+            Session session = Session.getDefaultInstance(props, null);
+            MimeMessage msg = new MimeMessage(session, inputStream);
+            MimeMessageParser parser = new MimeMessageParser(msg).parse();
+            //邮件唯一id
+            String messageId = parser.getMimeMessage().getMessageID();
+            System.out.println(messageId);
+            //发件人
+            String from = parser.getFrom();
+            System.out.println(from);
+            //收件人列表
+            List<Address> toArray = parser.getTo();
+            System.out.println(toArray);
+            //抄送人列表
+            List<Address> ccArray = parser.getCc();
+            System.out.println(ccArray);
+            //密送人列表
+            List<Address> bccArray = parser.getBcc();
+            System.out.println(bccArray);
+            //邮件发送时间
+            Date sendDate = parser.getMimeMessage().getSentDate();
+            System.out.println(sendDate);
+            //邮件主题
+            String subject = parser.getSubject();
+            System.out.println(subject);
+            //获取正文
+            String html = parser.getHtmlContent();
+            System.out.println(html);
+            //所有文件，包括附件和正文中的图片等文件
+            List<DataSource> dataSources = parser.getAttachmentList();
+            //获取不到html内容时，则获取非html文本内容
+            if (html == null || html.length() == 0) {
+                String plain = parser.getPlainContent();
+                System.out.println(plain);
+                msg.setContent(plain, "text/plain");
+            } else {
+                //获取正文中的图片等文件
+                Collection<String> contentIds = parser.getContentIds();
+                for (String contentId : contentIds) {
+                    DataSource contentFile = parser.findAttachmentByCid(contentId);
+                    dataSources.remove(contentFile);
+                    String name = contentFile.getName();
+                    InputStream is = contentFile.getInputStream();
+                    FileUtils.copyInputStreamToFile(is, new File(dir + name));
+                    html = html.replace("cid:" + contentId, name);
+                }
+                msg.setContent(html, "text/html");
+                FileUtils.writeStringToFile(new File(dir + "test.html"), html, Charset.defaultCharset());
+            }
+            for (DataSource dataSource : dataSources) {
+                String name = dataSource.getName();
+                System.out.println(name);
+                InputStream is = dataSource.getInputStream();
+                FileUtils.copyInputStreamToFile(is, new File(dir + name));
+            }
+            msg.saveChanges();
+            FileOutputStream fileOutputStream = new FileOutputStream(desFile);
+            msg.writeTo(fileOutputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
