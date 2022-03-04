403b07b228a3443f9ae705a66f28eca334b3542d
coolwhite520 <81929154@qq.com>
2022-03-04 17:11:48 +0800

    sss

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ mkdir "./target/encrypted"
 ./encrypt -encrypt -in ./target/trans-extends-0.0.1-SNAPSHOT.jar -out ./target/encrypted/app.jar
 docker build --network=host -t file:5.1.2 .
 docker save -o file.tar file:5.1.2
-#docker rmi file:5.1.2
+docker rmi file:5.1.2
